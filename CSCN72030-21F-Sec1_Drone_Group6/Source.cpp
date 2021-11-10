7107e9d50a0567e014fb1680541e6db696c45d8a
techee12 <70961125+techee12@users.noreply.github.com>
2021-11-10 10:02:25 -0500

    Temp

--- CSCN72030-21F-Sec1_Drone_Group6/Source.cpp
+++ CSCN72030-21F-Sec1_Drone_Group6/Source.cpp
@@ -2,8 +2,14 @@
 #include <iostream>
 #include "batteryWater.h"
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	connection* connect = new connection((char*) "A1", (char*) "a2");
-
+	
+		std::cout << "Hello, World!" << std::endl;
+		connection* connect = new connection((char*)"A1", (char*)"a2");
+	//try {
+	//	throw MyException();
+	//}
+	//catch (MyException e) {
+	//	cout << e.what();
+	//}
 	return 0;
 }
--- CSCN72030-21F-Sec1_Drone_Group6/connection.h
+++ CSCN72030-21F-Sec1_Drone_Group6/connection.h
@@ -44,6 +44,7 @@ private:
 
 // Exceptions
 struct fileNotOpened : public exception {
+	
 	const char* issue() const throw() {
 		return "Error Opening File";
 	}
@@ -66,3 +67,25 @@ struct renameFail : public exception {
 	}
 };
 
+
+//class MyException : public std::exception {
+//	const char* file;
+//	int line;
+//	const char* func;
+//	const char* info;
+//
+//public:
+//	MyException(const char* msg, const char* file_, int line_, const char* func_, const char* info_ = "") : std::exception(msg),
+//		file(file_),
+//		line(line_),
+//		func(func_),
+//		info(info_)
+//	{
+//	}
+//
+//	const char* get_file() const { return file; }
+//	int get_line() const { return line; }
+//	const char* get_func() const { return func; }
+//	const char* get_info() const { return info; }
+//
+//};
